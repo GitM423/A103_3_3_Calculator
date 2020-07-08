2483a9a721e398e29eac23289e65f4ddca5e01cc
GitM423 <matthias.p.hoeh@gmail.com>
2020-07-08 15:06:25 +0200

    v0.0.1

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+let body = document.getElementById('body')
+let input = document.getElementById('input')
+
+function enter(element) {
+    input.value += element
+}
+function calc() {
+    console.log("calc")
+    input.value = eval(input.value).toFixed(2)
+    console.log(input.value)
+}
+function del() {
+    console.log("del")
+    input.value = input.value.slice(0, -1);
+}
+function clr() {
+    console.log("clr")
+    input.value = null
+}
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault()
+        document.getElementById("senden").click()
+    }
+})
